a5acbf4d3ca9208b8231573fe1de16ba3c31e2f1
Kyo Takata <kyotakata1@gmail.com>
2024-11-28 02:33:46 +0900

    9.C02_DbContextの作成

---
+++ WinFormsApp2/MyEFCore/AndersonDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp2.MyEFCore
+{
+    /// <summary>
+    /// 接続したいデータベースの名前+DbContext
+    /// AmazonならAmazonDbContext,TSUTAYAならTSUTAYADbContext
+    /// システムの名前が入る
+    /// </summary>
+    public class AndersonDbContext : DbContext
+    {
+    }
+}
